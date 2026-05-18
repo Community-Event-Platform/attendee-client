9c76aa9e09c69abc6a0d24ab6c133c2bb86b919b
NhutNguyen25 <nguyentiennhut25102006@gmail.com>
2026-05-18 17:11:57 +0700

    finish about-contact

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
       </Routes>
 
       <Footer />
