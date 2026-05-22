5c81542982970f8230d6eb45365bc7a122338534
Dung1303 <nguyentdung1326@gmail.com>
2026-05-22 11:00:29 +0700

    fix: update import paths for Header and useAuth components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/layout/header/Header";
+import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import { Routes, Route } from "react-router-dom";
 import { useEffect, useCallback } from "react";
