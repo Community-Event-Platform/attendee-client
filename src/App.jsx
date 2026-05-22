c79e33aa530f6beafaa5006c17f2d8d808ad6e24
NhutNguyen25 <nguyentiennhut25102006@gmail.com>
2026-05-22 14:45:25 +0700

    finish layout

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Register from "./pages/Register";
 import OAuthSuccess from "./pages/OAuthSuccess";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
+import Event from "./pages/Event";
 
 function App() {
   const { initAuth } = useAuth();
@@ -34,6 +35,7 @@ function App() {
         <Route path="/login" element={<Login addToast={addToast} />} />
         <Route path="/register" element={<Register addToast={addToast} />} />
         <Route path="/oauth-success" element={<OAuthSuccess addToast={addToast} />} />
+        <Route path="/events" element={<Event addToast={addToast} />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
       </Routes>
